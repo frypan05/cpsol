b18b033ab22e18d37cc2f1eebe5098a249d4e2ea
Daksh Sharma <95771231+frypan05@users.noreply.github.com>
2024-06-28 12:53:27 +0530

    initial stack

---
+++ cpp practice/firstprog.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Now we'll code in neovim" << std::endl;
+  return 0;
+}
---
+++ cpp practice/programCompile.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "!g++ my_program.cpp -o my_program.exe" << std::endl;
+  return 0;
+}
